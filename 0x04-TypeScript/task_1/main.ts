7f221f5862951c7064a2639b228860bcc36f9072
enyasystem <enyaelvis@gmail.com>
2025-11-09 18:24:10 +0100

    Build Teacher interface with readonly, optional, and dynamic properties

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,50 @@
+//Define the Teacher interface
+interface Teacher {
+    readonly firstName: string; // readonly - can't change after initialization
+    readonly lastName: string; // same here also
+    fullTimeEmployee: boolean; //must always exist
+    yearsOfExperience?: number; // Optional
+    location: string;           //must always exist
+    [key: string]: any;         // index signature => allows extra propertities like "contract"
+}
+
+//Example object using the interface
+const teacher3: Teacher = {
+    firstName: 'Bernard',
+    fullTimeEmployee: false,
+    lastName: 'Igiri',
+    location: 'Ekureku',
+    contract: false,            // allowed because of the [key: string]: any
+};
+
+console.log(teacher3);
+
+// Directors interface extends Teacher and adds numberOfReports
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Example director object
+export const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
+
+// Function type for printing teacher name
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// Implementation of printTeacher
+export const printTeacher: printTeacherFunction = (
+    firstName: string,
+    lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+// Example usage
+console.log(printTeacher('John', 'Doe'));
