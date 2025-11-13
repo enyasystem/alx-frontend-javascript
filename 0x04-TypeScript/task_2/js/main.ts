e927a36f1e72308b6ba70050484a7d65d4dd3c61
enyasystem <enyaelvis@gmail.com>
2025-11-13 13:05:01 +0100

    refactor(task_2): change export to local for Subjects type and teachClass function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -85,9 +85,9 @@ executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
 
 // Part 7: String literal types and teachClass
-export type Subjects = 'Math' | 'History';
+type Subjects = 'Math' | 'History';
 
-export function teachClass(todayClass:Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === 'Math') {
     const res = 'Teaching Math';
     console.log(res);
